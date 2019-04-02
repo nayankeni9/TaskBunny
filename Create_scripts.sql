a0bf2ad3855514d1c6bb5c842e82402d75533d69
Nayan <nayankeni9@gmail.com>
2019-04-02 10:18:50 -0500

    Sign in and databases added

---
+++ Create_scripts.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, session
 import json
-
+from flask_mysqldb import MySQL, MySQLdb
+import bcrypt
+mysql = MySQL()
 app = Flask(__name__)
 # app._static_folder = 'static'
+app.config['MYSQL_HOST'] = 'remotemysql.com'
+app.config['MYSQL_USER'] = 'jsckp5HTKU'
+app.config['MYSQL_PASSWORD'] = '9O5x78oQvj'
+app.config['MYSQL_DB'] = 'jsckp5HTKU'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.secret_key = 'super_secret_key'
+mysql = MySQL(app)
 
 @app.route("/")
 def home():
@@ -14,11 +23,41 @@ def about():
 
 @app.route('/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        print('The url is here'+request.form['email']+'   '+request.form['password'])
-        return redirect(url_for('home'))
+    email= request.form['email']
+    password= request.form['password'].encode('utf-8')
+    
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("SELECT * FROM user where email=%s",(email,))
+    user = cur.fetchone()
+    cur.close()
+    print(type(user))
+    if user is not None:
+        if bcrypt.hashpw(password, user['password'].encode('utf-8')) == user['password'].encode('utf-8'):
+            session['name'] = user['firstname']
+            session['email'] = user['email']
+            return redirect(url_for('home'))
     else:
-        print('error')
+        return "Username or Password does not match"
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return render_template("index.html")
+
+@app.route('/register', methods=["POST"])
+def register():
+    firstname= request.form['firstname']
+    lastname= request.form['lastname']
+    email= request.form['email']
+    password= request.form['password'].encode('utf-8')
+    hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO user (firstname,lastname,email,password) VALUES(%s,%s,%s,%s)",(firstname,lastname,email,hash_password,))
+    mysql.connection.commit()
+    session['name']= firstname
+    session['email'] = email
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
